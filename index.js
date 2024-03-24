ddd9cd4904ee06dbb1d74710e02835466c90cd48
cnyaga66 <cnyga66@gmail.com>
2024-03-24 10:45:10 +0300

    My first arrow function

---
+++ index.js
@@ -0,0 +1,12 @@
+//let greet = function(){
+    //console.log(Hello World);
+    //return 1;
+//}
+
+//console.log(greet()); // returns Hello World and 1
+
+let add =(num1,num2) => num1 + num2;
+
+let result =add(5,8)
+
+console.log(result);
